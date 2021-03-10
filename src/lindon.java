75a28a9a840dde043ad237a8c1968b7900331e42
lindon1992 <522656037@qq.com>
2021-03-10 14:52:34 +0800

    init

---
+++ src/lindon.java
@@ -0,0 +1,7 @@
+public class lindon {
+    public static void main(String[] args) {
+        System.out.println("lindtt hahahah11133");
+        System.out.println("lindtt shsauysay7777444");
+
+    }
+}
